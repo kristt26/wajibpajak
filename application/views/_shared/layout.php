40992ee8d2a8f7b025bdc11721e718799e996a44
kristt26 <kristt26@gmail.com>
2020-11-20 19:02:10 +0900

    ok

--- application/views/_shared/layout.php
+++ application/views/_shared/layout.php
@@ -14,6 +14,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <title>Dashboard - Tabler - Premium and Open Source dashboard template with responsive and high quality UI.</title>
+  <link rel="shortcut icon" href="<?=base_url()?>favicon.ico" type="image/x-icon">
+  <link rel="icon" href="<?=base_url()?>favicon.ico" type="image/x-icon">
   <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
   <meta name="msapplication-TileColor" content="#206bc4" />
   <meta name="theme-color" content="#206bc4" />
