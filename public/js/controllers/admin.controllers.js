84e669df33b12fdb395b9996f1ac0c89854fa0a9
kristt26 <kristt26@gmail.com>
2020-11-20 18:45:05 +0900

    template

---
+++ public/js/controllers/admin.controllers.js
@@ -0,0 +1,10 @@
+angular.module('adminctrl', [])
+    .controller('pageController', pageController)
+    .controller('profileController', profileController)
+    ;
+function pageController($scope) {
+    $scope.Title = "Page Header";
+}
+function profileController($scope) {
+    $scope.title = "Profile Header";
+}
